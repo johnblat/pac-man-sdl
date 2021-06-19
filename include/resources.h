26b06bdb89be8b92fd3fdd97ec556eb69d9b17eb
John Blat <johnblat64@protonmail.com>
2021-06-19 14:15:50 -0400

    load from config declaration

--- include/resources.h
+++ include/resources.h
@@ -13,6 +13,8 @@ void load_global_texture_atlases_from_config_file( SDL_Renderer *renderer );
 void load_animations_from_config_file( AnimatedSprite **animated_sprites );
 
 void load_render_xx_from_config_file( RenderClipFromTextureAtlas **render_clips );
+
+void load_ghost_mode_times_from_config_file( uint8_t *ghost_mode_times, int num_periods );
 /**
  * FILE I/O FOR SAVING/LOADING
  */
