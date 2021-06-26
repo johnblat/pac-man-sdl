dc9bc0496a2f93d22bd9a81a532bc2c5d2907d08
John Blat <johnblat64@protonmail.com>
2021-06-26 18:51:13 -0400

    change input names

--- include/input.h
+++ include/input.h
@@ -3,14 +3,20 @@
 
 #include <SDL2/SDL_gamecontroller.h>
 
-SDL_GameController *g_GameController = NULL;
+
+
+SDL_GameController *g_GameControllers[] = {NULL, NULL};
+
+
 unsigned int g_NumJoysticks = 0;
 unsigned int g_NumGamepads = 0;
-unsigned int g_GameControllerButtonMask = 0b00000;
-unsigned int g_GAMEPAD_UP =               0b10000;
-unsigned int g_GAMEPAD_LEFT =             0b01000;
-unsigned int g_GAMEPAD_RIGHT =            0b00100;
-unsigned int g_GAMEPAD_DOWN =             0b00010;
-unsigned int g_GAMEPAD_ACTION =           0b00001;
+unsigned int g_NumFullKeyboards = 0; // ARROWS and Z
+unsigned int g_NumHalfKeyboards = 0; // NUMPAD and YGHJs
+unsigned int g_InputMasks[] = { 0b00000, 0b00000 };
+unsigned int g_INPUT_UP =                0b10000;
+unsigned int g_INPUT_LEFT =              0b01000;
+unsigned int g_INPUT_RIGHT =             0b00100;
+unsigned int g_INPUT_DOWN =              0b00010;
+unsigned int g_INPUT_ACTION =            0b00001;
 
 #endif
